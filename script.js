0ffe29be4d8e3b78d6dd131181d3d4f562582f53
anothervictoria <victoria.panidi@gmail.com>
2024-06-03 10:19:56 +0400

    some and every

--- script.js
+++ script.js
@@ -157,6 +157,20 @@ btnLogin.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -514,3 +528,22 @@ for (const acc of accounts) {
   }
 }
 console.log(accountFor);
+
+//Equality
+console.log(movements);
+console.log(movements.includes(-130));
+
+//Condition
+console.log(movements.some(mov => mov === 130));
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
