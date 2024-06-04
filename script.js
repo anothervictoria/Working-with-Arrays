0c39df3540519ec35bdc71239ea1a4b4c439127e
anothervictoria <victoria.panidi@gmail.com>
2024-06-04 09:54:41 +0400

    sorting arrays

--- script.js
+++ script.js
@@ -61,9 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -211,6 +214,13 @@ btnClose.addEventListener('click', function (e) {
     containerApp.style.opacity = 0;
   }
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
@@ -577,3 +587,27 @@ const overalBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance);
+
+// Sort strings
+// sort method mutates the original array
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+
+// Numbers are sorting according to if they were strings
+console.log(movements);
+console.log(movements.sort());
+
+// ascending
+movements.sort((a, b) => {
+  // return < 0, A, B
+  // return > 0, B, A
+  // if (a > b) return 1;
+
+  // if (b > a) return -1;
+  a - b;
+});
+console.log(movements);
+
+// decsending
+movements.sort((a, b) => b - a);
+console.log(movements);
